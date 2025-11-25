3a4b114d9389db11a1c1d101fdaa144ccc676145
bodand <bodand@proton.me>
2025-11-25 16:43:17 +0100

    Add constants for OIDFed Go-to-C bindings.

---
+++ constants.go
@@ -0,0 +1,30 @@
+package main
+
+/*
+static const char* const OidfedFederationSuffix = "/.well-known/openid-federation";
+
+static const char* const OidfedContentTypeEntityStatement = "application/entity-statement+jwt";
+static const char* const OidfedContentTypeTrustMark = "application/trust-mark+jwt";
+static const char* const OidfedContentTypeResolveResponse = "application/resolve-response+jwt";
+static const char* const OidfedContentTypeTrustChain = "application/trust-chain+json";
+static const char* const OidfedContentTypeTrustMarkDelegation = "application/trust-mark-delegation+jwt";
+static const char* const OidfedContentTypeJWKS = "application/jwk-set+jwk";
+static const char* const OidfedContentTypeExplicitRegistrationResponse = "application/explicit-registration-response+jwt";
+
+static const char* const OidfedJwtTypeEntityStatement = "entity-statement+jwt";
+static const char* const OidfedJwtTypeTrustMark = "trust-mark+jwt";
+static const char* const OidfedJwtTypeTrustMarkDelegation = "trust-mark-delegation+jwt";
+static const char* const OidfedJwtTypeResolveResponse = "resolve-response+jwt";
+static const char* const OidfedJwtTypeJWKS = "jwk-set+jwk";
+static const char* const OidfedJwtTypeExplicitRegistrationResponse = "explicit-registration-response+jwt";
+
+static const char* const OidfedEntityTypeFederationEntity = "federation_entity";
+static const char* const OidfedEntityTypeOpenIDRelyingParty = "openid_relying_party";
+static const char* const OidfedEntityTypeOpenIdProvider = "openid_provider";
+static const char* const OidfedEntityTypeOAuthClient = "oauth_client";
+static const char* const OidfedEntityTypeOAuthProtectedResource = "oauth_resource";
+
+static const char* const OidfedRegistrationTypeAutomatic = "automatic";
+static const char* const OidfedRegistrationTypeExplicit = "explicit";
+*/
+import "C"
